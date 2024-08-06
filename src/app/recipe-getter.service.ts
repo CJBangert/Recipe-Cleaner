ea99d536fbe26a4d0181322a94a9f1fb1d7a6249
cjbangert <cjbangert108@hotmail.com>
2024-08-06 14:27:44 -0400

    use innerHTML property binding to render

--- src/app/recipe-getter.service.ts
+++ src/app/recipe-getter.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, EventEmitter, Output } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 
 
@@ -16,9 +16,10 @@ export class RecipeGetterService {
 
 
   getContent(url:string): void   {
-    const apiCallUrl = `${this.backendUrl}/getShortRecipe/${url}`;
+    const apiCallUrl = `${this.backendUrl}/getShortRecipe`;
     console.log(apiCallUrl)
-    const response = this.http.get(apiCallUrl).subscribe(response => {
+    let params = new HttpParams().set('url', url);
+    const response = this.http.get(apiCallUrl, {params: params}).subscribe(response => {
       this.dataSubject.next(response);
     });
   }
