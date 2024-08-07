38ddcd255cf3542fc85497f9b343267851ef2c29
CJ <38147098+CJBangert@users.noreply.github.com>
2024-08-07 15:37:24 -0400

    add validation to the input, add error handling, improve subsription to the new way (#2)

--- src/app/url-input/url-input.component.ts
+++ src/app/url-input/url-input.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 import { RecipeGetterService } from '../recipe-getter.service';
 @Component({
   selector: 'app-url-input',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './url-input.component.html',
   styleUrl: './url-input.component.scss'
 })
 export class UrlInputComponent {
 
-  url: string = '';
-
+  urlForm: FormGroup;
+  
   constructor(
     private recipeService: RecipeGetterService,
-  ) {}
+    private fb: FormBuilder
+  ) {
+    let pattern = '^(https?:\\/\\/)?([\\da-z.-]+)\\.([a-z.]{2,6})([\\/\\w .-]*)*\\/?$'
+    this.urlForm = this.fb.group({
+      url: ['', [Validators.required, Validators.pattern(pattern)]]
+    });
+  }
 
   getRecipe(){
-    this.recipeService.getContent(this.url)
+    let url: string = this.urlForm.get('url')?.value
+    if(url != null){
+      this.recipeService.getContent(url)
+    }
+    
   }
-  
-
-
 }
