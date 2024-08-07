821a45ea741eb704148c994b919a85c51fa64da9
CJ <38147098+CJBangert@users.noreply.github.com>
2024-08-07 10:41:49 -0400

    add firebase (#1)
    * add firebase
    
    * add a little desc text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UrlInputComponent } from './url-input/url-input.component';
 import { DisplayRecipeComponent } from "./display-recipe/display-recipe.component";
+import { DescriptionBlockComponent } from "./description-block/description-block.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UrlInputComponent, DisplayRecipeComponent],
+  imports: [RouterOutlet, UrlInputComponent, DisplayRecipeComponent, DescriptionBlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
